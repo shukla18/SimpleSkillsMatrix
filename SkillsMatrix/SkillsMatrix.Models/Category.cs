bad680b8836f228ceb48d2b9d38471dc284fff5f
shukla18 <shantanu-shukla@hotmail.com>
2024-07-17 21:15:58 +0530

    First draft

---
+++ SkillsMatrix/SkillsMatrix.Models/Category.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillsMatrix.Models
+{
+    public class Category
+    {
+        public int? Id { get; set; }
+
+        /// <summary>
+        /// Name of the category.
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Description of the Category
+        /// </summary>
+        public string? Description { get; set; }
+
+        public Category? Parent { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
